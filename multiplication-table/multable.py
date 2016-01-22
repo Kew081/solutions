b00923e5cc55c7d0dbe9021d757ef11c4a1f3ec5
Jorge Gonzalez <jrgqhd@gmail.com>
2016-01-22 11:48:16 -0500

    Update multable.py

--- multiplication-table/multable.py
+++ multiplication-table/multable.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 def multable():
+    # prints easy to read multiplication table for numbers 1-12
     for num in range(1, 13):
         for i in range(1, 13):
             print(str(i * num).rjust(4), end='')
