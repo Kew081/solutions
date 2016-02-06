b10cb4a6390952f4060f21f10258201faafd07c7
Jorge Gonzalez <jrgqhd@gmail.com>
2016-02-06 18:53:05 -0500

    Update fib.py

--- fibonacci/fib.py
+++ fibonacci/fib.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
+
 def fib(n):
     # returns nth term in fibonacci sequence
     x, y = 0, 1
     for i in range(1, n):
         x, y = y, x + y
     return x
+
+if __name__ == '__main__':
+    print(fib(10))
